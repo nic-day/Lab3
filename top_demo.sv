7f89dfabc8b5bc5fd217f7ae0b5eacab74aa569a
nic-day <145714891+nic-day@users.noreply.github.com>
2023-11-01 17:10:51 -0500

    Add files via upload
    Post FPGA board.

---
+++ top_demo.sv
@@ -0,0 +1,73 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 01/15/2021 06:40:11 PM
+// Design Name: 
+// Module Name: top_demo
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module top_demo
+(
+  // input
+  input  logic [7:0] sw,
+  input  logic [3:0] btn,
+  input  logic       sysclk_125mhz,
+  input  logic       rst,
+  // output  
+  output logic [7:0] led,
+  output logic sseg_ca,
+  output logic sseg_cb,
+  output logic sseg_cc,
+  output logic sseg_cd,
+  output logic sseg_ce,
+  output logic sseg_cf,
+  output logic sseg_cg,
+  output logic sseg_dp,
+  output logic [3:0] sseg_an
+);
+
+  logic [16:0] CURRENT_COUNT;
+  logic [16:0] NEXT_COUNT;
+  logic        smol_clk;
+  logic [5:0]  y;
+  logic        clkdiv; 
+
+  // Place FSM instantiation here
+
+  clk_div dut_1(sysclk_125mhz, btn[0], clkdiv);
+  FSM dut(clkdiv, btn[1], sw[7], sw[6], y);
+  
+  assign {led[7:5],led[2:0]} = y;
+  assign led[4:3] = 2'b00;
+
+
+// Register logic storing clock counts
+  always@(posedge sysclk_125mhz)
+  begin
+    if(btn[3])
+      CURRENT_COUNT = 17'h00000;
+    else
+      CURRENT_COUNT = NEXT_COUNT;
+  end
+  
+  // Increment logic
+  assign NEXT_COUNT = CURRENT_COUNT == 17'd100000 ? 17'h00000 : CURRENT_COUNT + 1;
+
+  // Creation of smaller clock signal from counters
+  assign smol_clk = CURRENT_COUNT == 17'd100000 ? 1'b1 : 1'b0;
+
+endmodule
